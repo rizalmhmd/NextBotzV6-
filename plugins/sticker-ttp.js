25820605ba63d1345d281d7836a67462ff2e6c2a
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-01-16 19:19:55 +0700

    Update sticker-ttp.js

--- plugins/sticker-ttp.js
+++ plugins/sticker-ttp.js
@@ -2,7 +2,7 @@ import { sticker } from '../lib/sticker.js'
 
 let handler = async (m, { conn, text }) => {
     let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-    let stiker = await sticker(null, global.API('xteam', '/ttp', { file: '', text: teks }), global.packname, global.author)
+    let stiker = await sticker(null, global.API('lol', '/api/ttp', 'ee99d6df5f8c5eaf7667a90d', { file: '', text: teks }), global.packname, global.author)
     if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
     throw stiker.toString()
 }
