02e7578676f0974ee1f90e711fb5223e21e920e6
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-01-16 19:18:26 +0700

    Update sticker-attp2.js

--- plugins/sticker-attp2.js
+++ plugins/sticker-attp2.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
    let response = args.join(' ').split('|')
   if (!args[0]) throw 'ᴍᴀꜱᴜᴋᴋᴀɴ ᴛᴇxᴛ'
   m.reply('ᴘʀᴏꜱᴇꜱ...')
-  let res = `https://api.lolhuman.xyz/api/ttp2?apikey=64333e1c050ced435defe154&text=${response[0]}&apikey=Xynoz`
+  let res = `https://api.lolhuman.xyz/api/ttp2?apikey=357d3c85079c4e0818731e6b&text=${response[0]}&apikey=Xynoz`
   conn.sendFile(m.chat, res, 'xynz.webp', `ꜱᴜᴅᴀʜ ᴊᴀᴅɪ`, m, false)
 }
 handler.help = ['attp2 <teks>']
