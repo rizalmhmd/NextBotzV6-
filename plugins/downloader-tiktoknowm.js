e6abf05df217d97c830a89650e77b77fc120f251
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-02-03 17:56:56 +0700

    Update downloader-tiktoknowm.js

--- plugins/downloader-tiktoknowm.js
+++ plugins/downloader-tiktoknowm.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, usedPrefix, command, args }) => {
   if (!res.ok) throw await res.text()
 let json = await res.json()
    let cap = `Cara Simpan\n\n1. Download Dulu Vidio Nya Kalau Sudah\n2. Masuk Ke Dalam Vidio Nya\n3. Lalu Tekan Titik3 Pojok Kanan Atas\n4. Lalu Pilih Yang Ada Bacaan  Simpan `
-await conn.sendButtonVid(m.chat, json.result.video_original, cap, wm, 'ᴀᴜᴅɪᴏ', `.tiktokaudio ${args}`, m)
+await conn.sendFile(m.chat, json.result.video_original, cap, m)
 }
 handler.help = ['ttnowm']
 handler.tags = ['downloader']
