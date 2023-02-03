4096dfa50dfc826bd9961a33e5dd2ab8f9e05212
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-02-03 17:21:17 +0700

    Update sticker-attp.js

--- plugins/sticker-attp.js
+++ plugins/sticker-attp.js
@@ -1,10 +1,12 @@
+import { sticker } from '../lib/sticker.js'
 let handler = async (m, { conn, text }) => {
     let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-    conn.sendFile(m.chat, global.API('lol', '/api/attp', 'ee99d6df5f8c5eaf7667a90d', { file: '', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
+    let stiker = await sticker(null, global.API('https://api.erdwpe.com/api/maker/', 'attp?text', { file: '', text: teks }), global.packname, global.author)
+    if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+    throw stiker.toString()
 }
 handler.help = ['attp <teks>']
 handler.tags = ['sticker']
-
 handler.command = /^attp$/i
-
-export default handler
+handler.limit = true
+export default handler 
