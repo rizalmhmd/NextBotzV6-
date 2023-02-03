aef31add5cea5e1a0997ad8f7e27e3804b7868ed
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-02-03 17:54:45 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, usedPrefix, command, args }) => {
    let res = await fetch(`https://saipulanuar.ga/api/download/tiktok?url=${args[0]}`)
   if (!res.ok) throw await res.text()
 let json = await res.json()
-    conn.sendHydrated(m.chat, `${htki} ᴛɪᴋᴛᴏᴋ ᴡᴍ ${htka}`, `➔ ɴɪᴄᴋɴᴀᴍᴇ ${nickname}${description ? `\n➔ ᴅᴇsᴄʀɪᴘᴛɪᴏɴ:\n${description}` : ''}`, await (await fetch(url)).buffer(),
+    conn.sendHydrated(m.chat, `Cara Simpan\n\n1. Download Dulu Vidio Nya Kalau Sudah\n2. Masuk Ke Dalam Vidio Nya\n3. Lalu Tekan Titik3 Pojok Kanan Atas\n4. Lalu Pilih Yang Ada Bacaan  Simpan ` : ''}`, await (await fetch(url)).buffer(),
         url, '🌎 s ᴏ ᴜ ʀ ᴄ ᴇ', null, null, [
         ['ᴅᴏɴᴀꜱɪ', `.donasi`],
         ['ᴀᴜᴅɪᴏ', `.tiktokaudio ${args}`],
