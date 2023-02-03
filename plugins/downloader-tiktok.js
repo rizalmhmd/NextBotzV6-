b319618251fbbf6456331dc8faeb2cec6cb3f1f5
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-02-03 17:02:26 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,14 +1,10 @@
-import { tiktokdl, tiktokdlv2, tiktokdlv3 } from '@bochilteam/scraper'
 import fetch from 'node-fetch'
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-
-  const { author: { nickname }, video, description } = await tiktokdl(args[0])
-        .catch(async _ => await tiktokdlv2(args[0]))
-        .catch(async _ => await tiktokdlv3(args[0]))
-    const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
-    if (!url) throw 'Can\'t download video!'
+let handler = async (m, { conn, usedPrefix, command, args }) => {
+   if (!args[0]) return m.reply(`Masukan Url TikTok !`)
+   let res = await fetch(`https://saipulanuar.ga/api/download/tiktok?url=${args[0]}`)
+  if (!res.ok) throw await res.text()
+let json = await res.json()
     conn.sendHydrated(m.chat, `${htki} ᴛɪᴋᴛᴏᴋ ᴡᴍ ${htka}`, `➔ ɴɪᴄᴋɴᴀᴍᴇ ${nickname}${description ? `\n➔ ᴅᴇsᴄʀɪᴘᴛɪᴏɴ:\n${description}` : ''}`, await (await fetch(url)).buffer(),
         url, '🌎 s ᴏ ᴜ ʀ ᴄ ᴇ', null, null, [
         ['ᴅᴏɴᴀꜱɪ', `.donasi`],
